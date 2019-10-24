40e5dbb5b1a999ce546fdfb09e8169df38937292
Romain Descamps <rdescamps@unis.fr>
2019-10-24 17:35:48 +0200

    JavaBean lib

--- Bean/src/factory/RapportActiviteFactory.java
+++ Bean/src/factory/RapportActiviteFactory.java
@@ -4,23 +4,69 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import bean.RapportActiviteOm;
 import bean.RapportActivite;
 import bean.RapportActiviteActivites;
 import bean.RapportActiviteConges;
+import bean.RapportActiviteOm;
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 public class RapportActiviteFactory {
+	
+	private static Collection<RapportActiviteConges> loadConges() {
+		
+		List<RapportActiviteConges> listConges=new ArrayList<>();
+		RapportActiviteConges conges=new RapportActiviteConges();
+		
+		String intituleOm = "test";
+		String valeurMille = "1000";
+		String jour = "1";
+		conges.setIntituleConge(intituleOm);
+		conges.setJour(jour);
+		conges.setValeurMille(valeurMille);
+		listConges.add(conges);
+		return listConges;
+	}
 
-	public static Collection<RapportActivite> load(){
+	private static Collection<RapportActiviteActivites> loadActivites() {
+		
+		List<RapportActiviteActivites> listActivites=new ArrayList<>();
+		RapportActiviteActivites activites=new RapportActiviteActivites();
+		
+		String intituleOm = "test";
+		String valeurMille = "1000";
+		String jour = "2";
+		activites.setIntituleOm(intituleOm);
+		activites.setJour(jour);
+		activites.setValeurMille(valeurMille);
+		
+		listActivites.add(activites);
+		
+		
+		return listActivites;
+	}
+
+	private static Collection<RapportActiviteOm> loadOm() {
+		
+		List<RapportActiviteOm> listOm=new ArrayList<>();
+		RapportActiviteOm om=new RapportActiviteOm();
+		
+	    String nomOm = "bidon";
+		om.setNomOm(nomOm);
+		
+		listOm.add(om);
+			
+		return listOm;
+	}
+
+	public static Collection<RapportActivite> loadFactory(){
 		
 		List<RapportActivite> rapportList=new ArrayList<RapportActivite>();
 		RapportActivite rapport = new RapportActivite();
-		String mois = null;
-		String annee = null;
-		String nomCollaborateur = null;
-		String prenomCollaborateur = null;
+		String mois = "9";
+		String annee = "2019";
+		String nomCollaborateur = "test";
+		String prenomCollaborateur = "test";
 		
 		rapport.setMois(mois);
 		rapport.setAnnee(annee);
@@ -45,51 +91,7 @@ public class RapportActiviteFactory {
 		
 	}
 
-	private static Collection<RapportActiviteConges> loadConges() {
-		
-		List<RapportActiviteConges> listConges=new ArrayList<>();
-		RapportActiviteConges conges=new RapportActiviteConges();
-		
-		String intituleOm = null;
-		String valeurMille = null;
-		String jour = null;
-		conges.setIntituleConge(intituleOm);
-		conges.setJour(jour);
-		conges.setValeurMille(valeurMille);
-		listConges.add(conges);
-		return listConges;
-	}
 
-	private static Collection<RapportActiviteActivites> loadActivites() {
-		
-		List<RapportActiviteActivites> listActivites=new ArrayList<>();
-		RapportActiviteActivites activites=new RapportActiviteActivites();
-		
-		String intituleOm = null;
-		String valeurMille = null;
-		String jour = null;
-		activites.setIntituleOm(intituleOm);
-		activites.setJour(jour);
-		activites.setValeurMille(valeurMille);
-		
-		listActivites.add(activites);
-		
-		
-		return listActivites;
-	}
-
-	private static Collection<RapportActiviteOm> loadOm() {
-		
-		List<RapportActiviteOm> listOm=new ArrayList<>();
-		RapportActiviteOm om=new RapportActiviteOm();
-		
-	    String nomOm = null;
-		om.setNomOm(nomOm);
-		
-		listOm.add(om);
-			
-		return listOm;
-	}
 	
 	
 	
