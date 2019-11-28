7d3b75ff30fa2d9bef5d4111de74ba7adc0d87b2
Romain Descamps <rdescamps@unis.fr>
2019-11-28 11:43:54 +0100

    Ajout projet server

---
+++ JasperServer/src/main/java/Main/JasperServerApplication.java
@@ -0,0 +1,13 @@
+package Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperServerApplication.class, args);
+	}
+
+}
---
+++ JasperServer/src/test/java/Main/JasperServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
